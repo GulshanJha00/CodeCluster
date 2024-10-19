314fcc14d4fb41fe204d6ec015d58b6eec9e38ca
Gulshan Kumar <gulshankumar@Gulshans-MacBook-Air.local>
2024-10-19 18:20:26 +0530

    Heading animation done

--- script.js
+++ script.js
@@ -36,3 +36,52 @@ gsap.to("#card h1", {
         pin:true,
     }
 })
+
+
+
+
+
+
+
+
+
+
+var h1 = document.querySelector(".heading");
+var h1Text = h1.textContent;
+console.log(h1Text)
+
+var splitText = h1Text.split("");
+var length = splitText.length/2;
+console.log(length);
+
+
+var cluster = ""
+splitText.forEach(function(e,idx){
+    if(idx < length){
+
+        cluster += `<span class= "a">${e}</span>`
+    }
+    else{
+        cluster += `<span class="b">${e}</span>`
+    }
+})
+h1.innerHTML = cluster;
+console.log(cluster);
+
+
+
+gsap.from("#nav h2 .a",{
+    y : 50,
+    opacity : 0,
+    duration : 0.8,
+    delay : 0.5,
+    stagger : 0.15,
+})
+
+gsap.from("#nav h2 .b",{
+    y : 50,
+    opacity : 0,
+    duration : 0.8,
+    delay : 0.5,
+    stagger : -0.15,
+})
